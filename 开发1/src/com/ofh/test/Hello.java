7598e8d31aa640ae0efcb45754fcc629425b3bfe
kent <hongrenqu77@vip.qq.com>
2021-10-17 15:56:25 +0800

    [v1.6]third change

--- 开发1/src/com/ofh/test/Hello.java
+++ 开发1/src/com/ofh/test/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello World-2021-10-17");
         System.out.println("修改1");
-        System.out.println("修改2");
+        System.out.println("修改3");
     }
 }
