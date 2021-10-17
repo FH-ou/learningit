8dc25681e69435def4c6d05458c63529b97896fc
kent <hongrenqu77@vip.qq.com>
2021-10-17 15:50:34 +0800

    [v1.6]first change

--- 开发1/src/com/ofh/test/Hello.java
+++ 开发1/src/com/ofh/test/Hello.java
@@ -3,5 +3,6 @@ package com.ofh.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello World-2021-10-17");
+        System.out.println("修改1");
     }
 }
